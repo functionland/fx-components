d720644a5bc9a30546f39127c5a4aa494738f578
buchansm <shane@buchan.io>
2022-07-21 11:37:40 -0400

    Added missing card types

--- libs/component-library/src/lib/card/card.tsx
+++ libs/component-library/src/lib/card/card.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { ViewProps } from 'react-native';
 import { BoxProps } from '@shopify/restyle';
 import { FxBox } from '../box/box';
 import { FxTheme } from '../theme/theme';
 
-export const FxCard = (props: BoxProps<FxTheme>) => (
+type FxCardProps = BoxProps<FxTheme> & ViewProps;
+
+export const FxCard = (props: FxCardProps) => (
   <FxBox
     {...props}
     marginHorizontal="16"
