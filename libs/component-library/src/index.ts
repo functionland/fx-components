33d25af450f151e857e256493d9fbc082a6b4f3d
buchansm <shane@buchan.io>
2022-07-21 13:44:13 -0400

    Created `FxBoxProps` type

--- libs/component-library/src/index.ts
+++ libs/component-library/src/index.ts
@@ -1,4 +1,4 @@
-export { FxBox } from './lib/box/box';
+export { FxBox, FxBoxProps } from './lib/box/box';
 export { FxButton } from './lib/button/button';
 export { FxCard } from './lib/card/card';
 export { FxChevronDownIcon, FxDownArrowIcon } from './lib/icons/icons';
