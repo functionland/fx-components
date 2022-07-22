850bd60b6bc666817da6532fd83cf6c5c767e86b
buchansm <shane@buchan.io>
2022-07-22 18:11:29 -0400

    Revised `<FxError />` styling/props

--- libs/component-library/src/lib/error/error.tsx
+++ libs/component-library/src/lib/error/error.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { TextProps } from '@shopify/restyle';
+import { BoxProps } from '@shopify/restyle';
 import { FxText } from '../text/text';
 import { FxTheme } from '../theme/theme';
 import { FxExclamationIcon } from '../icons/icons';
 import { FxBox } from '../box/box';
 
-type FxErrorProps = TextProps<FxTheme> & {
+type FxErrorProps = BoxProps<FxTheme> & {
   error?: string;
 };
 
@@ -14,7 +14,7 @@ const FxError = ({ error, ...rest }: FxErrorProps) => {
   return (
     <FxBox marginTop="8" flexDirection="row" alignItems="center" {...rest}>
       <FxExclamationIcon color="errorBase" />
-      <FxText color="errorBase" marginLeft="4">
+      <FxText variant="bodyXSRegular" color="errorBase" marginLeft="4">
         {error}
       </FxText>
     </FxBox>
