81a2853c2a8717c82427f4da3b69bd48f3fd8a14
JDMathew <justindavidmathew@gmail.com>
2022-08-05 17:42:04 +0100

    Removing unused screens

--- apps/box/src/screens/Settings/index.ts
+++ apps/box/src/screens/Settings/index.ts
@@ -2,6 +2,4 @@ export * from './Settings.screen';
 export * from './About.screen';
 export * from './Mode.screen';
 export * from './ConnectedDApps.screen';
-export * from './Invitations.screen';
 export * from './Pools.screen';
-export * from './WalletSettings.screen';
