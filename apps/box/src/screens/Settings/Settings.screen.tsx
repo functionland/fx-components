fae7a720b1fa471507bb18a7701fe8fb0beddb11
JDMathew <justindavidmathew@gmail.com>
2022-08-05 17:50:11 +0100

    Update Settings screen

--- apps/box/src/screens/Settings/Settings.screen.tsx
+++ apps/box/src/screens/Settings/Settings.screen.tsx
@@ -1,10 +1,22 @@
-import { FxBox, FxText } from '@functionland/component-library';
+import {
+  FxBox,
+  FxButton,
+  FxHorizontalRule,
+} from '@functionland/component-library';
+
 import React from 'react';
+import { SettingsMenu } from '../../components/SettingsList';
+import { HeaderText } from '../../components/Text';
+import Version from '../../components/Version';
 
 export const SettingsScreen = () => {
   return (
-    <FxBox>
-      <FxText variant="body">Settings Dashboard</FxText>
+    <FxBox marginHorizontal="20">
+      <HeaderText>Settings</HeaderText>
+      <SettingsMenu />
+      <FxHorizontalRule marginVertical="16" />
+      <FxButton size={'large'}>{'Log out'}</FxButton>
+      <Version marginTop="16" />
     </FxBox>
   );
 };
