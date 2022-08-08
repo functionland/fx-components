26e8a2de3eca3eb45b37dab67833635d6454bbcf
JDMathew <justindavidmathew@gmail.com>
2022-08-08 18:36:17 +0100

    Upate mode Slice - Added persist storage   - Async   - Serialization   - Deserialization   - rehydration function
    - Added store functions
      - hydrations
      - modes (get, auto, colorscheme)

--- apps/box/src/stores/useSettingsStore.ts
+++ apps/box/src/stores/useSettingsStore.ts
@@ -1,18 +1,60 @@
 import create, { StateCreator } from 'zustand';
+import { persist } from 'zustand/middleware';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useColorScheme } from 'react-native';
 
 type Modes = ['light', 'dark', 'auto'];
+type ColorScheme = 'light' | 'dark';
 type Mode = Modes[number];
 interface ModeSlice {
+  _hasHydrated: boolean;
+  setHasHydrated: (isHydrated: boolean) => void;
   modes: Modes;
   currentMode: Mode;
-  setMode: (mode: Mode) => void;
+  getMode: () => ColorScheme;
+  isAuto: boolean;
+  colorScheme: ColorScheme;
+  setColorScheme: (colorScheme: ColorScheme) => void;
 }
-const createModeSlice: StateCreator<ModeSlice, [], [], ModeSlice> = (set) => ({
-  modes: ['light', 'dark', 'auto'],
-  currentMode: 'auto',
-  setMode: (mode: Mode) =>
-    set((state) => ({ currentMode: (state.currentMode = mode) })),
-});
+const createModeSlice: StateCreator<
+  ModeSlice,
+  [],
+  [['zustand/persist', ModeSlice]],
+  ModeSlice
+> = persist(
+  (set, get) => ({
+    _hasHydrated: false,
+    setHasHydrated: (isHydrated) => {
+      set({
+        _hasHydrated: isHydrated,
+      });
+    },
+    modes: ['light', 'dark', 'auto'],
+    currentMode: 'auto',
+    isAuto: true,
+    colorScheme: 'light',
+    setColorScheme: (colorScheme: ColorScheme) =>
+      set(() => ({ colorScheme: colorScheme })),
+    switchIsAuto: () => set((state) => ({ isAuto: !state.isAuto })),
+    getMode: () => {
+      const { isAuto, colorScheme } = get();
+      const systemColorScheme = useColorScheme(); // eslint-disable-line react-hooks/rules-of-hooks
+      return isAuto ? systemColorScheme : colorScheme;
+    },
+  }),
+  {
+    name: 'mode',
+    getStorage: () => AsyncStorage,
+    serialize: (state) => JSON.stringify(state),
+    deserialize: (str) => JSON.parse(str),
+    onRehydrateStorage: () => {
+      // anything to run before rehydrating, return function is called after rehydrating
+      return (state) => {
+        state.setHasHydrated(true);
+      };
+    },
+  }
+);
 
 export const useSettingsStore = create<ModeSlice>()((...a) => ({
   ...createModeSlice(...a),
