5c595822243d524a7e235d62c8def52994e56a1b
JDMathew <justindavidmathew@gmail.com>
2022-08-10 00:54:48 +0100

    Added Group Radio button Comp

---
+++ libs/component-library/src/lib/radio-button/RadioButtonGroup.tsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import { View } from 'react-native';
+
+type Props = {
+  /**
+   * Function to execute on selection change.
+   */
+  onValueChange: (value: string) => void;
+  /**
+   * Value of the currently selected radio button.
+   */
+  value: string;
+  /**
+   * React elements containing radio buttons.
+   */
+  children: React.ReactNode;
+};
+
+export type RadioButtonContextType = {
+  value: string;
+  onValueChange: (item: string) => void;
+};
+
+const initialContext: RadioButtonContextType = {
+  value: '',
+  onValueChange: () => {}, // eslint-disable-line @typescript-eslint/no-empty-function
+};
+
+export const RadioButtonContext =
+  React.createContext<RadioButtonContextType>(initialContext);
+
+export const useRadioButtonContext = () => React.useContext(RadioButtonContext);
+
+/**
+ * Radio button group allows to control a group of radio buttons.
+ *
+ * ## Usage
+ * ```jsx
+ * import React from 'react';
+ * import { View } from 'react-native';
+ * import { RadioButton, Text } from 'components';
+ *
+ * const MyComponent = () => {
+ *   const [value, setValue] = React.useState('first');
+ *
+ *   return (
+ *     <RadioButton.Group onValueChange={newValue => setValue(newValue)} value={value}>
+ *       <View>
+ *         <Text>First</Text>
+ *         <RadioButton value="first" />
+ *       </View>
+ *       <View>
+ *         <Text>Second</Text>
+ *         <RadioButton value="second" />
+ *       </View>
+ *     </RadioButton.Group>
+ *   );
+ * };
+ *
+ * export default MyComponent;
+ *```
+ */
+const RadioButtonGroup = ({ value, onValueChange, children }: Props) => (
+  <RadioButtonContext.Provider value={{ value, onValueChange }}>
+    <View accessibilityRole="radiogroup">{children}</View>
+  </RadioButtonContext.Provider>
+);
+
+RadioButtonGroup.displayName = 'RadioButton.Group';
+export default RadioButtonGroup;
+
+export { RadioButtonGroup };
