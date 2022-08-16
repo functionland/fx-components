e2399efd58d8cf62595da6cbf1fea8c7c22a5209
Justin D Mathew <justindavidmathew@gmail.com>
2022-08-16 18:08:31 +0100

    Adding GridSelector to component lib (#181)
    * Adding GridSelector to component lib
    
    * Update exports
    
    * Add disabled to gridSelector
    
    * Add Layout Animations to gridList Comp

---
+++ libs/component-library/src/lib/grid-selector/gridSelector.tsx
@@ -0,0 +1,50 @@
+import { useTheme } from '@shopify/restyle';
+import React from 'react';
+import { FxBox } from '../box/box';
+import { GridIcon, ListIcon } from '../Icons';
+import { FxPressableOpacity } from '../pressable-opacity/pressableOpacity';
+import { FxSpacer } from '../spacer/spacer';
+import { FxTheme } from '../theme/theme';
+import { configureEaseInOutLayoutAnimation } from '../utils/animations';
+
+export type FxGridSelectorProps = {
+  isList: boolean;
+  disabled?: boolean;
+  animated?: boolean;
+  setIsList: React.Dispatch<React.SetStateAction<boolean>>;
+} & React.ComponentProps<typeof FxBox>;
+
+export const FxGridSelector = ({
+  isList,
+  setIsList,
+  disabled,
+  animated = true,
+  ...rest
+}: FxGridSelectorProps) => {
+  const { colors } = useTheme<FxTheme>();
+  const disabledColor = disabled ? colors.backgroundPrimary : undefined;
+
+  const setIsListWithAnimation = (bool: boolean) => () => {
+    setIsList(bool);
+    animated && configureEaseInOutLayoutAnimation();
+  };
+  return (
+    <FxBox alignItems={'center'} flexDirection="row" {...rest}>
+      <FxPressableOpacity
+        disabled={disabled}
+        onPress={setIsListWithAnimation(true)}
+      >
+        <ListIcon fill={isList ? colors.greenBase : colors.content3} />
+      </FxPressableOpacity>
+      <FxSpacer width={12} />
+      <FxPressableOpacity
+        disabled={disabled}
+        onPress={setIsListWithAnimation(false)}
+      >
+        <GridIcon
+          fill={disabledColor || (isList ? colors.content3 : colors.greenBase)}
+        />
+      </FxPressableOpacity>
+    </FxBox>
+  );
+};
