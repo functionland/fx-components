f110f69a4c0ddef83298072d00b746f69e842020
JDMathew <justindavidmathew@gmail.com>
2022-08-18 15:13:12 +0100

    Add type helpers for: - WithPartial - copy type and update with partial (optional) types - RequireOnly - make all partial with specific required

--- libs/component-library/src/lib/types.ts
+++ libs/component-library/src/lib/types.ts
@@ -3,3 +3,7 @@ export type $RemoveChildren<T extends React.ComponentType> = $Omit<
   React.ComponentPropsWithoutRef<T>,
   'children'
 >;
+export type WithPartial<T, K extends keyof T> = Omit<T, K> &
+  Partial<Pick<T, K>>;
+export type RequireOnly<T, K extends keyof T> = Partial<T> &
+  Required<Pick<T, K>>;
