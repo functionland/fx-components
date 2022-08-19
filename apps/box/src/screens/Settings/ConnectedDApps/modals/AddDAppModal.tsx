db9f9a53d316dd1853296454551c781047f6b3a8
JDMathew <justindavidmathew@gmail.com>
2022-08-19 18:26:05 +0100

    Create AddDAppModal Component

---
+++ apps/box/src/screens/Settings/ConnectedDApps/modals/AddDAppModal.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import {
+  FxBottomSheetModal,
+  FxBottomSheetModalMethods,
+  FxBox,
+} from '@functionland/component-library';
+import { imageMap } from './../../../../api/connectedDApps';
+import { SubHeaderText } from './../../../../components/Text';
+import { DoneButton, DAppHeader } from '../components';
+
+const addDAppModalData = [
+  {
+    imageSrc: imageMap.fileSync,
+    name: 'File Sync',
+    info: 'Get the most out of your blox by connecting to dApps',
+  },
+  {
+    imageSrc: imageMap.fotos,
+    name: 'Fotos',
+    info: 'Backup photos from your device to the pool to your Blox hardware',
+  },
+];
+
+type AddDAppModalProps = {};
+const AddDAppModal = React.forwardRef<
+  FxBottomSheetModalMethods,
+  AddDAppModalProps
+>(({ ...props }, ref) => {
+  return (
+    <FxBottomSheetModal ref={ref}>
+      <FxBox>
+        <SubHeaderText textAlign="center" marginVertical={'24'}>
+          Get the most out of your blox by connecting to dApps
+        </SubHeaderText>
+        {addDAppModalData.map((data) => (
+          <DAppHeader key={data.name} marginBottom="32" {...data} />
+        ))}
+        <DoneButton />
+      </FxBox>
+    </FxBottomSheetModal>
+  );
+});
+
+export default AddDAppModal;
