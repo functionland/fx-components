aa97557608d497f8b51a7085c451d0ea58242fa6
Keith Luchtel <keith@keithluchtel.com>
2022-08-24 11:12:05 -0500

    Add Auto Width Tabs component
    Adds Auto Width Tabs component and updates the Tabs Demo screen.

--- libs/component-library/src/lib/tabs/tabs.tsx
+++ libs/component-library/src/lib/tabs/tabs.tsx
@@ -1,56 +1,94 @@
-import { useTheme } from '@shopify/restyle';
 import React from 'react';
 import { FxBox, FxReanimatedBox } from '../box/box';
-import { FxPressableOpacity } from '../pressable-opacity/pressableOpacity';
-import { FxText, FxTextProps } from '../text/text';
-import { FxTheme } from '../theme/theme';
+import {
+  FxPressableOpacity,
+  FxPressableOpacityProps,
+} from '../pressable-opacity/pressableOpacity';
+import { FxText } from '../text/text';
 import {
   useAnimatedReaction,
   useAnimatedStyle,
-  useDerivedValue,
   useSharedValue,
   withTiming,
 } from 'react-native-reanimated';
+import { FxSpacer } from '../spacer/spacer';
+import { StyleSheet, View } from 'react-native';
+import { ColorProps } from '@shopify/restyle';
+import { FxTheme } from '../theme/theme';
 
-const ANIMATION_DURATION = 300;
-const SELECTED_TEXT: FxTextProps = { color: 'greenBase' };
-const PRESSED_TEXT: FxTextProps = { color: 'backgroundSecondary' };
+const ANIMATION_DURATION = 150;
 
-type FixedTabProps = {
+type ColorOverrideType = ColorProps<FxTheme> & {
+  color: keyof FxTheme['colors'];
+};
+
+const SELECTED_TEXT: ColorOverrideType = { color: 'greenBase' };
+const AUTO_PRESSED: ColorOverrideType = { color: 'border' };
+const FIXED_PRESSED: ColorOverrideType = {
+  color: 'backgroundSecondary',
+};
+
+const FIXED_TAB_STYLES: FxPressableOpacityProps = {
+  borderBottomWidth: 2,
+  borderBottomColor: 'content3',
+  paddingVertical: '16',
+};
+
+const AUTO_TAB_STYLES: FxPressableOpacityProps = {
+  paddingVertical: '8',
+};
+
+const VARIANT_STYLES = {
+  fixed: {
+    defaults: FIXED_TAB_STYLES,
+    pressedText: FIXED_PRESSED,
+    pressedBackground: {
+      backgroundColor: 'backgroundPrimary',
+      borderBottomColor: 'backgroundSecondary',
+    } as FxPressableOpacityProps,
+  },
+  auto: {
+    defaults: AUTO_TAB_STYLES,
+    pressedText: AUTO_PRESSED,
+    pressedBackground: {} as FxPressableOpacityProps,
+  },
+};
+
+type TabProps = {
   text: string;
   selected: boolean;
   onPress: () => void;
+  variant: 'auto' | 'fixed';
 };
 
-const FixedTab = ({ text, selected, onPress }: FixedTabProps) => {
-  const theme = useTheme<FxTheme>();
+const Tab = ({ text, selected, onPress, variant }: TabProps) => {
   const [pressed, setPressed] = React.useState<boolean>(false);
 
-  let textOverrides = pressed ? PRESSED_TEXT : {};
+  const backgroundOverrides = pressed
+    ? VARIANT_STYLES[variant].pressedBackground
+    : {};
+
+  let textOverrides = pressed ? VARIANT_STYLES[variant].pressedText : {};
   textOverrides = { ...textOverrides, ...(selected ? SELECTED_TEXT : {}) };
 
   return (
     <FxPressableOpacity
-      flex={1}
       onPress={onPress}
-      alignItems="center"
-      justifyContent="center"
-      borderBottomWidth={2}
-      borderBottomColor="content3"
-      paddingVertical="16"
       disabled={selected}
       hitSlop={0}
+      alignItems="center"
+      justifyContent="center"
       onPressIn={() => {
         setPressed(true);
       }}
       onPressOut={() => {
         setPressed(false);
       }}
+      {...VARIANT_STYLES[variant].defaults}
+      {...backgroundOverrides}
       style={(args) => [
         args.pressed && {
           opacity: 1,
-          backgroundColor: theme.colors.backgroundPrimary,
-          borderBottomColor: theme.colors.backgroundSecondary,
         },
       ]}
     >
@@ -67,6 +105,7 @@ type FxTabsProps = {
   onSelect: (idx: number) => void;
   animate?: boolean;
   animationDuration?: number; // ms
+  variant?: 'fixed' | 'auto';
 };
 export const FxTabs = ({
   items,
@@ -74,12 +113,48 @@ export const FxTabs = ({
   onSelect,
   animate = true,
   animationDuration = ANIMATION_DURATION,
+  variant = 'fixed',
 }: FxTabsProps) => {
   const translateX = useSharedValue(0);
-  const containerWidth = useSharedValue<number>(0);
-  const highlightWidth = useDerivedValue(() => {
-    return containerWidth.value / items.length;
-  }, [containerWidth, items]);
+  const highlightWidth = useSharedValue<number>(0);
+  const selectedLayout = useSharedValue<{ x: number; width: number }>({
+    x: 0,
+    width: 0,
+  });
+  const itemLayouts = React.useRef<Array<{ x: number; width: number }>>([]);
+
+  React.useEffect(() => {
+    if (itemLayouts.current[selectedIdx]) {
+      selectedLayout.value = itemLayouts.current[selectedIdx];
+    }
+  }, [selectedIdx]); // eslint-disable-line react-hooks/exhaustive-deps
+
+  const tabsItems = items.map((item, idx) => {
+    return (
+      <View
+        key={idx}
+        style={styles[variant]}
+        onLayout={(event) => {
+          const { x, width } = event.nativeEvent.layout;
+          itemLayouts.current[idx] = {
+            x: x,
+            width: width,
+          };
+
+          if (idx === selectedIdx) {
+            selectedLayout.value = { x: x, width: width };
+          }
+        }}
+      >
+        <Tab
+          text={item}
+          selected={idx === selectedIdx}
+          onPress={() => selectHandler(idx)}
+          variant={variant}
+        />
+      </View>
+    );
+  });
 
   // This handles computing what value to translate to and detecting
   // if we have had a chance to measure the width of the Tab Bar. This
@@ -88,15 +163,20 @@ export const FxTabs = ({
   useAnimatedReaction(
     () => {
       return {
-        translateValue: highlightWidth.value * selectedIdx,
-        hasMeasured: containerWidth.value > 0,
+        translateValue: selectedLayout.value.x,
+        width: selectedLayout.value.width,
+        hasMeasured: selectedLayout.value.width > 0,
       };
     },
     (data, prevData) => {
       if (!prevData?.hasMeasured || !animate) {
         translateX.value = data.translateValue;
+        highlightWidth.value = data.width;
       } else {
         translateX.value = withTiming(data.translateValue, {
+          duration: animationDuration,
+        });
+        highlightWidth.value = withTiming(data.width, {
           duration: animationDuration,
         });
       }
@@ -117,22 +197,22 @@ export const FxTabs = ({
   });
 
   return (
-    <FxBox
-      flexDirection="row"
-      onLayout={(event) => {
-        containerWidth.value = event.nativeEvent.layout.width;
-      }}
-    >
-      {items.map((item, idx) => {
-        return (
-          <FixedTab
-            text={item}
-            key={idx}
-            selected={idx === selectedIdx}
-            onPress={() => selectHandler(idx)}
-          />
-        );
-      })}
+    <FxBox flexDirection="row">
+      {variant === 'auto'
+        ? tabsItems.reduce((acc, cur) => {
+            if (!acc) {
+              return cur;
+            } else {
+              return (
+                <>
+                  {acc}
+                  <FxSpacer width={24} />
+                  {cur}
+                </>
+              );
+            }
+          })
+        : tabsItems}
       <FxReanimatedBox
         position="absolute"
         height={2}
@@ -143,3 +223,10 @@ export const FxTabs = ({
     </FxBox>
   );
 };
+
+const styles = StyleSheet.create({
+  fixed: {
+    flex: 1,
+  },
+  auto: {},
+});
