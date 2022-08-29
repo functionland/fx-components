f8b87a5bff5dd228b6e09c195eafa00e291a2436
Keith Luchtel <keith@keithluchtel.com>
2022-08-29 14:58:54 -0500

    Rewrite Progress Bar to use Reanimated
    Rewrites the Progress Bar component to use Reanimated to fix some
    odd opacity issues on Android.

--- libs/component-library/src/lib/progress-bar/progressBar.tsx
+++ libs/component-library/src/lib/progress-bar/progressBar.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
+import { LayoutChangeEvent } from 'react-native';
+import {
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from 'react-native-reanimated';
 
-import { FxBox, FxBoxProps } from '../box/box';
-import { configureEaseInOutLayoutAnimation } from '../utils/animations';
+import { FxBox, FxBoxProps, FxReanimatedBox } from '../box/box';
 
 export type FxProgressBarProps = {
   progress: number;
   width?: number;
   total?: number;
 } & FxBoxProps;
+
+const calculateWidth = (progress: number, total: number, width: number) => {
+  const _progress = Math.min(Math.max(0, progress), total);
+  return Math.round(width * (_progress / total));
+};
 
 const FxProgressBar = ({
   width,
@@ -17,12 +27,31 @@ const FxProgressBar = ({
   ...rest
 }: FxProgressBarProps) => {
   const [_width, setWidth] = React.useState(width || 0);
-  const _progress = Math.min(Math.max(0, progress), total);
-  const progressWidth = Math.round(_width * (_progress / total));
+  const progressWidth = useSharedValue<number>(0);
 
   React.useEffect(() => {
-    configureEaseInOutLayoutAnimation();
-  }, [progressWidth]);
+    const newWidth = calculateWidth(progress, total, _width);
+    if (newWidth !== progressWidth.value) {
+      progressWidth.value = withTiming(newWidth, {
+        duration: 150,
+      });
+    }
+  }, [progress, total, _width]); // eslint-disable-line react-hooks/exhaustive-deps
+
+  const progressWidthStyle = useAnimatedStyle(() => {
+    return {
+      width: progressWidth.value,
+    };
+  });
+
+  const onLayoutHandler = (evt: LayoutChangeEvent) => {
+    setWidth(evt.nativeEvent.layout.width);
+    progressWidth.value = calculateWidth(
+      progress,
+      total,
+      evt.nativeEvent.layout.width
+    );
+  };
 
   return (
     <FxBox
@@ -31,17 +60,15 @@ const FxProgressBar = ({
       backgroundColor={'backgroundSecondary'}
       width={width}
       height={height}
-      onLayout={(evt) => {
-        setWidth(evt.nativeEvent.layout.width);
-      }}
+      onLayout={onLayoutHandler}
       borderRadius={'m'}
       {...rest}
     >
-      <FxBox
+      <FxReanimatedBox
         height={height}
-        width={progressWidth}
         backgroundColor={'greenHover'}
         borderRadius={'m'}
+        style={progressWidthStyle}
       />
     </FxBox>
   );
