901a9f58f2abc1c190a33d92bac22612db39f796
Keith Luchtel <keith.luchtel@formidable.com>
2022-08-30 19:19:49 -0500

    Add Auto Width Tabs component (#208)
    * Add Auto Width Tabs component
    
    Adds Auto Width Tabs component and updates the Tabs Demo screen.
    
    * Update auto variant text variant
    
    Updates the the text variant used for auto width tabs.
    
    * PR feedback
    
    Implement PR feedback.

--- apps/box/src/screens/Settings/ComponentGallery/TabsDemo.screen.tsx
+++ apps/box/src/screens/Settings/ComponentGallery/TabsDemo.screen.tsx
@@ -5,28 +5,44 @@ import {
   FxTabs,
   FxText,
 } from '@functionland/component-library';
+import { HeaderText, SubHeaderText } from '../../../components/Text';
 import React from 'react';
 
-const TAB_ITEMS = ['first', 'second'];
+const TAB_ITEMS = ['first', 'second', 'third', 'fourth'];
 
 export const TabsDemoScreen = () => {
-  const [selectedIdx, setSelectedIdx] = React.useState<number>(0);
-
-  const selectHandler = (idx: number) => {
-    setSelectedIdx(idx);
-  };
+  const [fixedIdx, setFixedIdx] = React.useState<number>(2);
+  const [autoIdx, setAutoIdx] = React.useState<number>(0);
 
   return (
     <FxSafeAreaBox marginHorizontal="20" flex={1}>
-      <FxText variant="h200">Tabs Demo</FxText>
+      <HeaderText>Tabs Demo</HeaderText>
       <FxSpacer marginTop="16" />
-      <FxTabs
-        items={TAB_ITEMS}
-        selectedIdx={selectedIdx}
-        onSelect={selectHandler}
-      />
-      <FxBox alignItems="center" justifyContent="center" flex={1}>
-        <FxText variant="h300">{TAB_ITEMS[selectedIdx]}</FxText>
+      <FxBox flex={1}>
+        <SubHeaderText>Fixed width tabs</SubHeaderText>
+        <FxTabs
+          items={TAB_ITEMS}
+          selectedIdx={fixedIdx}
+          onSelect={(idx) => setFixedIdx(idx)}
+          animate={true}
+          variant="fixed"
+        />
+        <FxBox alignItems="center" justifyContent="center" flex={1}>
+          <FxText variant="h300">{TAB_ITEMS[fixedIdx]}</FxText>
+        </FxBox>
+      </FxBox>
+      <FxBox flex={1}>
+        <SubHeaderText>Auto width tabs</SubHeaderText>
+        <FxTabs
+          items={TAB_ITEMS}
+          selectedIdx={autoIdx}
+          onSelect={(idx) => setAutoIdx(idx)}
+          animate={true}
+          variant="auto"
+        />
+        <FxBox alignItems="center" justifyContent="center" flex={1}>
+          <FxText variant="h300">{TAB_ITEMS[autoIdx]}</FxText>
+        </FxBox>
       </FxBox>
     </FxSafeAreaBox>
   );
