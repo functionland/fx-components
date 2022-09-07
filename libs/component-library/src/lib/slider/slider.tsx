00ead893c1ecf2de21708ae189671642afdd3d37
Shane Buchan <shane@buchan.io>
2022-09-07 17:11:10 -0400

    Rebuilt/Redesigned `<FxSlider />`

--- libs/component-library/src/lib/slider/slider.tsx
+++ libs/component-library/src/lib/slider/slider.tsx
@@ -1,42 +1,215 @@
 import React from 'react';
-import {
-  BoxProps,
-  boxRestyleFunctions,
-  composeRestyleFunctions,
-  createRestyleComponent,
-  useRestyle,
-} from '@shopify/restyle';
+import { BoxProps } from '@shopify/restyle';
 import { FxTheme } from '../theme/theme';
-import Slider, { SliderProps } from '@react-native-community/slider';
-import { FxBox } from '../box/box';
-import { useFxTheme } from '../theme/useFxTheme';
+import { SliderProps } from '@react-native-community/slider';
+import { FxBox, FxReanimatedBox } from '../box/box';
+import { StyleSheet } from 'react-native';
+import {
+  PanGestureHandler,
+  PanGestureHandlerGestureEvent,
+} from 'react-native-gesture-handler';
+import {
+  runOnJS,
+  useAnimatedGestureHandler,
+  useAnimatedStyle,
+  useDerivedValue,
+  useSharedValue,
+} from 'react-native-reanimated';
+import { clamp } from 'react-native-redash';
+import { FxText } from '../text/text';
 
-type FxSliderProps = BoxProps<FxTheme> & SliderProps;
+const THUMB_SIZE = 24;
 
-const FxSliderBase = createRestyleComponent<FxSliderProps, FxTheme>(
-  boxRestyleFunctions,
-  Slider
-);
+type Bounds = {
+  low: number; // screen coord
+  high: number; // screen coord
+};
 
-const restyleFunctions = composeRestyleFunctions<FxTheme, FxSliderProps>(
-  boxRestyleFunctions
-);
+type FxSliderProps = BoxProps<FxTheme> &
+  Pick<
+    SliderProps,
+    | 'disabled'
+    | 'value'
+    | 'onValueChange'
+    | 'minimumValue'
+    | 'maximumValue'
+    | 'step'
+  > & {
+    label?: string;
+  };
 
-const FxSlider = (props: FxSliderProps) => {
-  const { style, ...rest } = useRestyle(restyleFunctions, props);
-  const { colors } = useFxTheme();
+// converts x coordinate to step percent
+const toStep = (x: number, boundsX: Bounds, range: Bounds, step: number) => {
+  'worklet';
+  if (step === 0) return x / boundsX.high;
+  const stepX = step / (range.high - range.low);
+  return Math.round(x / boundsX.high / stepX) * stepX;
+};
+
+const FxSlider = ({
+  disabled,
+  value,
+  onValueChange,
+  minimumValue = 0,
+  maximumValue = 1,
+  step = 0,
+  label,
+  ...rest
+}: FxSliderProps) => {
+  const boundsX = useSharedValue<Bounds>({ low: 0, high: 0 });
+  const range = useDerivedValue(() => ({
+    low: minimumValue,
+    high: maximumValue,
+  }));
+  const _translateX = useSharedValue(0);
+  const translateX = useDerivedValue(() => {
+    if (typeof value === 'undefined') return _translateX.value;
+    return (
+      ((value - range.value.low) / (range.value.high - range.value.low)) *
+      boundsX.value.high
+    );
+  });
+  const labelWidth = useSharedValue(0);
+  const labelOpacity = useSharedValue(0);
+
+  const activePanHandler = (val: number) => {
+    if (onValueChange) onValueChange(val);
+  };
+
+  const onGestureEvent = useAnimatedGestureHandler<
+    PanGestureHandlerGestureEvent,
+    {
+      offsetX: number;
+    }
+  >({
+    onStart: (_, ctx) => {
+      ctx.offsetX = translateX.value;
+      labelOpacity.value = 1;
+    },
+    onActive: (event, ctx) => {
+      _translateX.value = clamp(
+        ctx.offsetX + event.translationX,
+        boundsX.value.low,
+        boundsX.value.high
+      );
+
+      const _value = clamp(
+        Math.round(
+          toStep(_translateX.value, boundsX.value, range.value, step) *
+            (range.value.high - range.value.low) +
+            range.value.low
+        ),
+        range.value.low,
+        range.value.high
+      );
+
+      runOnJS(activePanHandler)(_value);
+    },
+    onEnd: () => (labelOpacity.value = 0),
+  });
+
+  const positionerStyle = useAnimatedStyle(() => ({
+    transform: [{ translateX: translateX.value }],
+  }));
+
+  const trackStyle = useAnimatedStyle(() => ({
+    width: `${((translateX.value / boundsX.value.high) * 100).toFixed(2)}%`,
+  }));
+
+  const labelStyle = useAnimatedStyle(() => ({
+    transform: [
+      { translateX: -labelWidth.value / 2 + THUMB_SIZE / 2 },
+      { translateY: -10 },
+    ],
+    opacity: labelOpacity.value,
+  }));
 
   return (
-    <FxBox style={style}>
-      <FxSliderBase
+    <FxBox
+      width="100%"
+      height={THUMB_SIZE}
+      justifyContent="center"
+      opacity={disabled ? 0.5 : 1}
+      pointerEvents={disabled ? 'none' : 'auto'}
+      onLayout={(e) => {
+        const layout = e.nativeEvent.layout;
+        boundsX.value = {
+          low: 0,
+          high: layout.width - THUMB_SIZE,
+        };
+      }}
+      {...rest}
+    >
+      {/**
+       * Track
+       */}
+      <FxBox
         width="100%"
-        minimumTrackTintColor={colors.greenBase}
-        maximumTrackTintColor={colors.backgroundSecondary}
-        thumbTintColor={colors.greenBase}
-        {...rest}
-      />
+        height={4}
+        borderRadius="s"
+        backgroundColor="backgroundSecondary"
+      >
+        <FxReanimatedBox
+          height="100%"
+          borderRadius="s"
+          backgroundColor="greenBase"
+          style={trackStyle}
+        />
+      </FxBox>
+      {/**
+       * Positioner
+       */}
+      <PanGestureHandler onGestureEvent={onGestureEvent}>
+        <FxReanimatedBox position="absolute" style={positionerStyle}>
+          {/**
+           * Label
+           */}
+          <FxReanimatedBox
+            backgroundColor="backgroundSecondary"
+            borderRadius="s"
+            padding="8"
+            style={[labelStyle, styles.label]}
+            onLayout={(e) => {
+              const layout = e.nativeEvent.layout;
+              labelWidth.value = layout.width;
+            }}
+          >
+            <FxText variant="bodyXSRegular" color="content1" numberOfLines={1}>
+              {value}
+              {label ? ` ${label}` : ''}
+            </FxText>
+          </FxReanimatedBox>
+          {/**
+           * Thumb
+           */}
+          <FxBox backgroundColor="greenBase" style={styles.thumb}>
+            <FxBox backgroundColor="white" style={styles.thumbDot} />
+          </FxBox>
+        </FxReanimatedBox>
+      </PanGestureHandler>
     </FxBox>
   );
 };
+
+const styles = StyleSheet.create({
+  thumb: {
+    alignItems: 'center',
+    borderRadius: THUMB_SIZE / 2,
+    height: THUMB_SIZE,
+    justifyContent: 'center',
+    position: 'absolute',
+    top: -THUMB_SIZE / 2,
+    width: THUMB_SIZE,
+  },
+  thumbDot: {
+    borderRadius: THUMB_SIZE / 6,
+    height: THUMB_SIZE / 3,
+    width: THUMB_SIZE / 3,
+  },
+  label: {
+    bottom: THUMB_SIZE / 2,
+    position: 'absolute',
+  },
+});
 
 export { FxSlider };
