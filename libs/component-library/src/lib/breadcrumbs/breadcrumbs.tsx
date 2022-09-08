4e91abf333d802e622dd900a2f09c36c790cf094
JDMathew <justindavidmathew@gmail.com>
2022-09-08 17:25:39 +0200

    PR fixes

--- libs/component-library/src/lib/breadcrumbs/breadcrumbs.tsx
+++ libs/component-library/src/lib/breadcrumbs/breadcrumbs.tsx
@@ -20,30 +20,37 @@ export const FxBreadcrumbs = ({ path, ...rest }: FxBreadcrumbsProps) => {
 
   return (
     <FxBox flexDirection="row" alignItems="center" {...rest}>
-      {path.map((item, idx) => (
-        <>
-          <FxPressableOpacity
-            onPress={() => item.onPress(item)}
-            alignItems="center"
-            justifyContent="center"
-          >
-            <FxText variant="bodyXSRegular" color="content1">
-              {item.label}
-            </FxText>
-          </FxPressableOpacity>
-          {idx !== path.length - 1 && (
-            <>
-              <FxSpacer width={6} />
-              <FxChevronRightIcon
-                fill={colors.content1}
-                width="16"
-                height="16"
-              />
-              <FxSpacer width={6} />
-            </>
-          )}
-        </>
-      ))}
+      {path.map((item, idx) => {
+        const isLast = idx === path.length - 1;
+
+        return (
+          <FxBox key={`crumb-${idx}`} flexDirection="row" alignItems="center">
+            <FxPressableOpacity
+              onPress={() => item.onPress(item)}
+              alignItems="center"
+              justifyContent="center"
+            >
+              <FxText
+                variant={isLast ? 'bodyXSSemibold' : 'bodyXSRegular'}
+                color="content1"
+              >
+                {item.label}
+              </FxText>
+            </FxPressableOpacity>
+            {!isLast && (
+              <>
+                <FxSpacer width={6} />
+                <FxChevronRightIcon
+                  fill={colors.content1}
+                  width="16"
+                  height="16"
+                />
+                <FxSpacer width={6} />
+              </>
+            )}
+          </FxBox>
+        );
+      })}
     </FxBox>
   );
 };
