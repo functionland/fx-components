6d69784a43d23dbec1a7a306db967bfab3d4858b
guodevguru <guobrian118@gmail.com>
2022-10-02 10:23:41 -0400

    update devices screen

--- apps/box/src/screens/Devices.screen.tsx
+++ apps/box/src/screens/Devices.screen.tsx
@@ -1,10 +1,37 @@
-import { FxBox, FxText } from '@functionland/component-library';
-import React from 'react';
+import React, { useState } from 'react';
+import { FlatList, StyleSheet } from 'react-native';
+import {
+  FxBox,
+  FxHeader,
+  FxSafeAreaBox,
+  FxSpacer,
+  FxText,
+} from '@functionland/component-library';
+import { DeviceCard } from '../components';
+import { mockHub } from '../api/hub';
 
 export const DevicesScreen = () => {
+  const [isList, setIsList] = useState<boolean>(false);
+
   return (
-    <FxBox>
-      <FxText variant="body">Devices Dashboard</FxText>
-    </FxBox>
+    <FxSafeAreaBox flex={1} edges={['top']}>
+      <FxBox paddingHorizontal="20" paddingVertical="12">
+        <FxText variant="h300">Connected Devices</FxText>
+        <FxSpacer marginTop="24" />
+        <FxHeader title="All Cards" isList={isList} setIsList={setIsList} />
+      </FxBox>
+      <FlatList
+        contentContainerStyle={styles.devicesList}
+        data={[mockHub[0]]}
+        keyExtractor={(item) => item.name}
+        renderItem={({ item }) => <DeviceCard data={item} marginBottom="16" />}
+      />
+    </FxSafeAreaBox>
   );
 };
+
+const styles = StyleSheet.create({
+  devicesList: {
+    padding: 20,
+  },
+});
