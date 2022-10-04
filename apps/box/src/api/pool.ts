54a371a6cd1788e0b479c9071f0eb7ab0044cd06
guodevguru <guobrian118@gmail.com>
2022-10-03 21:39:09 -0400

    update blox settings pools screen

--- apps/box/src/api/pool.ts
+++ apps/box/src/api/pool.ts
@@ -4,8 +4,15 @@ export interface Pool {
   location: string;
 }
 
-export const mockPoolData: Pool = {
-  poolId: 'Pool_0982345454',
-  connectionDate: new Date().valueOf(),
-  location: '15 miles',
-};
+export const mockPoolData: Pool[] = [
+  {
+    poolId: 'Pool_0982345454',
+    connectionDate: new Date().valueOf(),
+    location: '15 miles',
+  },
+  {
+    poolId: 'Pool_0982345455',
+    connectionDate: new Date().valueOf(),
+    location: '51 miles',
+  },
+];
--- apps/box/src/screens/Settings/Pools.screen.tsx
+++ apps/box/src/screens/Settings/Pools.screen.tsx
@@ -1,11 +1,29 @@
-import { FxBox } from '@functionland/component-library';
-import React from 'react';
-import { SubHeaderText } from '../../components/Text';
+import React, { useState } from 'react';
+import Reanimated from 'react-native-reanimated';
+import { FxBox, FxHeader } from '@functionland/component-library';
+import { PoolCard } from '../../components/Cards/PoolCard';
+import { mockPoolData } from '../../api/pool';
 
 export const PoolsScreen = () => {
+  const [isList, setIsList] = useState<boolean>(false);
+
   return (
-    <FxBox marginHorizontal="20">
-      <SubHeaderText>Pools</SubHeaderText>
-    </FxBox>
+    <Reanimated.ScrollView nestedScrollEnabled={true}>
+      <FxBox paddingHorizontal="20" paddingVertical="20">
+        <FxHeader
+          title="Pools"
+          marginBottom="16"
+          isList={isList}
+          setIsList={setIsList}
+        />
+        <Reanimated.FlatList
+          data={mockPoolData}
+          keyExtractor={(item) => item.poolId}
+          renderItem={({ item }) => (
+            <PoolCard pool={item} isDetailed={!isList} />
+          )}
+        />
+      </FxBox>
+    </Reanimated.ScrollView>
   );
 };
