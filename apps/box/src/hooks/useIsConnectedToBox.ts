704d6be4ea766b381bb83e0b8792ceefc1f4b48d
Mahdi Ghorbani <mahdi.ghorbani@gmail.com>
2023-01-16 19:20:41 +0100

    improve useIsConnectedToBox hook

--- apps/box/src/hooks/useIsConnectedToBox.ts
+++ apps/box/src/hooks/useIsConnectedToBox.ts
@@ -1,34 +1,19 @@
 import { useState, useEffect } from 'react';
 import { isEmulatorSync } from 'react-native-device-info';
 import WifiManager from 'react-native-wifi-reborn';
-
+import { useNetInfo, NetInfoStateType } from '@react-native-community/netinfo'
 export const DEFAULT_NETWORK_NAME = 'Box';
 const TIMEOUT = 5000;
 
 export function useIsConnectedToBox() {
   const [isConnected, setIsConnected] = useState(false);
-
+  const networkInfo = useNetInfo()
   useEffect(() => {
-    let timeout = null;
-    const checkNetwork = () => {
-      WifiManager.getCurrentWifiSSID()
-        .then((actualSsid) => {
-          if (actualSsid === DEFAULT_NETWORK_NAME) setIsConnected(true);
-          else setIsConnected(false);
-
-          timeout = setTimeout(checkNetwork, TIMEOUT);
-        })
-        .catch(() => {
-          setIsConnected(false);
-          timeout = setTimeout(checkNetwork, TIMEOUT);
-        });
-    };
-    if (!isEmulatorSync()) checkNetwork();
-
-    return () => {
-      if (timeout) clearTimeout(timeout);
-    };
-  }, [setIsConnected]);
+    if (networkInfo.isConnected && networkInfo.type === NetInfoStateType.wifi)
+      setIsConnected(networkInfo?.details?.ssid === DEFAULT_NETWORK_NAME)
+    else
+      setIsConnected(false)
+  }, [networkInfo]);
 
   return isConnected;
 }
