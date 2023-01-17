44fc55e6fb9cff63c354f2cfea748675163a4053
Mahdi Ghorbani <mahdi.ghorbani@gmail.com>
2023-01-17 19:05:51 +0100

    set initial route

--- apps/box/src/navigation/Root.navigator.tsx
+++ apps/box/src/navigation/Root.navigator.tsx
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { InitialSetupNavigator } from './InitialSetup.navigator';
 import { MainTabsNavigator } from './MainTabs.navigator';
 import { Routes, RootStackParamList } from './navigationConfig';
+import { useUserProfileStore } from '../stores/useUserProfileStore';
 
 export const RootNavigator = () => {
+  const [_hasHydrated, appPeerId] = useUserProfileStore(state => [state._hasHydrated, state.appPeerId]);
+  const [initialRoute, setInitialRoute] = useState(undefined);
+  useEffect(() => {
+    if (_hasHydrated && !initialRoute) {
+      if (appPeerId) {
+        setInitialRoute(Routes.MainTabs);
+      } else {
+        setInitialRoute(Routes.InitialSetup);
+      }
+    }
+  }, [_hasHydrated, appPeerId, initialRoute])
+
+  if (!initialRoute)
+    return null;
   return (
-    <RootStack.Navigator screenOptions={{ gestureEnabled: true }}>
+    <RootStack.Navigator initialRouteName={initialRoute} screenOptions={{ gestureEnabled: true }}>
       <RootStack.Screen
         name={Routes.InitialSetup}
         options={{ headerShown: false }}
