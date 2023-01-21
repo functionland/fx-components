31151f768fd2d36d9582a7dd572878b6d48317fe
Mahdi Ghorbani <mahdi.ghorbani@gmail.com>
2023-01-21 18:07:25 +0100

    Setup deep linking on android

--- apps/box/src/navigation/NavContainer.tsx
+++ apps/box/src/navigation/NavContainer.tsx
@@ -1,16 +1,34 @@
 import React from 'react';
-import { DefaultTheme, NavigationContainer } from '@react-navigation/native';
+import { DefaultTheme, NavigationContainer, LinkingOptions } from '@react-navigation/native';
 import { useFxTheme } from '@functionland/component-library';
+import { Routes } from './navigationConfig';
 
 type NavContainerProps = {
   children?: React.ReactNode | string;
 };
-
+const linking: LinkingOptions<unknown> = {
+  enabled: true,
+  prefixes: ['fxblox://'],
+  config: {
+    screens: {
+      [Routes.MainTabs]:{
+        screens:{
+          [Routes.SettingsTab]:{
+            screens:{
+              [Routes.ConnectedDApps]: '/connectdapp/:appName/:bundleId/:peerId'
+            }
+          }
+        }
+      }
+    }
+  }
+}
 export const NavContainer = ({ children }: NavContainerProps) => {
   const theme = useFxTheme();
 
   return (
     <NavigationContainer
+      linking={linking}
       theme={{
         ...DefaultTheme,
         colors: {
