835a17d2b6875e3753902bfc218f9ba6e9de289a
Mahdi Ghorbani <mahdi.ghorbani@gmail.com>
2023-03-02 16:46:55 +0100

    get pools from blockchain

--- apps/box/src/screens/Settings/Pools.screen.tsx
+++ apps/box/src/screens/Settings/Pools.screen.tsx
@@ -1,12 +1,21 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet } from 'react-native';
 import Reanimated from 'react-native-reanimated';
 import { FxHeader } from '@functionland/component-library';
 import { PoolCard } from '../../components/Cards/PoolCard';
 import { mockPoolData } from '../../api/pool';
-
+import {blockchain} from '@functionland/react-native-fula'
 export const PoolsScreen = () => {
   const [isList, setIsList] = useState<boolean>(false);
+  useEffect(()=>{
+    loadPools()
+  },[])
+  
+  const loadPools=async ()=>{
+    //console.log('blockchain.init',blockchain.init())
+    const pools=await blockchain.listPools()
+    console.log('listPools',pools)
+  }
 
   return (
     <Reanimated.FlatList
