d0f42c93fe52c3710d3556b8446efcb837f45f21
Mahdi Ghorbani <mahdi.ghorbani@gmail.com>
2023-05-02 12:33:23 +0200

    Added build number to the version

--- apps/box/src/components/Version.tsx
+++ apps/box/src/components/Version.tsx
@@ -1,16 +1,17 @@
 import { FxText, FxTextProps } from '@functionland/component-library';
 import React from 'react';
-import { getVersion } from 'react-native-device-info';
+import { getVersion, getBuildNumber } from 'react-native-device-info';
 
 const Version = (props: FxTextProps) => {
   const version = getVersion();
+  const buildNumber = getBuildNumber()
   return (
     <FxText
       textAlign="center"
       variant="bodyXXSRegular"
       color="content3"
       {...props}
-    >{`Functionland Blox App version ${version}`}</FxText>
+    >{`Functionland Blox App version ${version} #${buildNumber}`}</FxText>
   );
 };
 
