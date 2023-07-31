f451fe457af17b9e778d876854826470cacef7df
Mahdi Ghorbani <mahdi.ghorbani@gmail.com>
2023-07-31 10:19:48 +0200

    Fixed convertByteToCapacityUnit bug

--- libs/component-library/src/lib/utils/conversions.ts
+++ libs/component-library/src/lib/utils/conversions.ts
@@ -20,13 +20,13 @@ const convertByteToCapacityUnit = (bytes: number, isBinary?: boolean) => {
   const constant = isBinary
     ? MEGABYTE_BINARY_CONSTANT
     : MEGABYTE_DECIMAL_CONSTANT;
-  const tera = bytes / (constant * 1000 * 1000 * 1000 * 1000);
-  if (tera > 1) return tera.toFixed(2) + ' TB';
-  const giga = bytes / (constant * 1000 * 1000 * 1000);
-  if (giga > 1) return giga.toFixed(2) + ' GB';
-  const mega = bytes / (constant * 1000 * 1000);
-  if (mega > 1) return mega.toFixed(2) + ' MB';
-  const kilo = bytes / (constant * 1000);
+  const tera = bytes / (constant * constant * constant * constant);
+  if (tera >= 1) return tera.toFixed(2) + ' TB';
+  const giga = bytes / (constant * constant * constant);
+  if (giga >= 1) return giga.toFixed(2) + ' GB';
+  const mega = bytes / (constant * constant);
+  if (mega >= 1) return mega.toFixed(2) + ' MB';
+  const kilo = bytes / constant;
   return kilo.toFixed(2) + ' KB';
 };
 export {
