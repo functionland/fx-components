7a4c575c2840b0d1dce7236b8c1034308ff0e599
Homayoun <hhio618@gmail.com>
2024-01-10 01:31:05 +0330

    Feat pools screen (#367)
    * Fix wallet connect error
    
    * finish pools screen

--- apps/box/src/screens/Settings/Pools.screen.tsx
+++ apps/box/src/screens/Settings/Pools.screen.tsx
@@ -1,46 +1,99 @@
 import React, { useEffect, useState } from 'react';
 import { StyleSheet } from 'react-native';
 import Reanimated from 'react-native-reanimated';
-import { FxHeader } from '@functionland/component-library';
+import {
+  FxBox,
+  FxHeader,
+  FxText,
+  FxTextInput,
+} from '@functionland/component-library';
 import { PoolCard } from '../../components/Cards/PoolCard';
 import { usePoolsStore } from '../../stores/usePoolsStore';
 import { shallow } from 'zustand/shallow';
+import MyLoader from '../../components/ContentLoader';
+import { SearchBar } from 'react-native-screens';
+import { range } from 'lodash';
+
 export const PoolsScreen = () => {
   const [isList, setIsList] = useState<boolean>(false);
-  const [pools, joinPool, getPools, leavePool] = usePoolsStore(
-    (state) => [state.pools, state.joinPool, state.getPools, state.leavePool],
+  const [isLoaded, setIsLoaded] = useState<boolean>(false);
+  const [isError, setIsError] = useState<boolean>(false);
+  const [search, setSearch] = useState<string>('');
+  const [pools, joinPool, cancelPoolJoin, getPools, leavePool] = usePoolsStore(
+    (state) => [
+      state.pools,
+      state.joinPool,
+      state.cancelPoolJoin,
+      state.getPools,
+      state.leavePool,
+    ],
     shallow
   );
+
+  const onChangeSearch = (query) => setSearch(query ? query : '');
+
   useEffect(() => {
-    getPools();
-  }, [getPools]);
+    setIsLoaded(false);
+    setIsError(false);
+    getPools(search)
+      .then((_) => {
+        setIsLoaded(true);
+        setIsError(false);
+      })
+      .catch((e) => {
+        setIsLoaded(false);
+        setIsError(true);
+        console.log('error getting pools: ', e);
+      });
+  }, [search]);
   console.log(pools);
 
+  if (isError) {
+    return (
+      <FxText variant="bodyMediumRegular" textAlign="center" fontSize={24}>
+        Error getting list of pools!
+      </FxText>
+    );
+  }
   return (
     <Reanimated.FlatList
       contentContainerStyle={styles.list}
       ListHeaderComponent={
-        <FxHeader
-          title="Pools"
-          marginBottom="16"
-          isList={isList}
-          setIsList={setIsList}
-        />
+        <FxBox>
+          <FxHeader
+            title="Pools"
+            marginBottom="16"
+            isList={isList}
+            setIsList={setIsList}
+          />
+          <FxTextInput
+            placeholder="Search pools..."
+            onChangeText={onChangeSearch}
+            value={search}
+          />
+        </FxBox>
       }
-      data={pools}
+      data={
+        isLoaded ? pools : Array.from({ length: 5 }, (_value, index) => index)
+      }
       keyExtractor={(item) => item.poolID}
-      renderItem={({ item }) => (
-        <PoolCard
-          pool={item}
-          isDetailed={!isList}
-          isRequested={item.requested}
-          isJoined={item.joined}
-          numVotes={item.numVotes}
-          numVoters={item.numVoters}
-          leavePool={leavePool}
-          joinPool={joinPool}
-        />
-      )}
+      renderItem={({ item }) =>
+        !isLoaded ? (
+          <MyLoader />
+        ) : (
+          <PoolCard
+            pool={item}
+            isDetailed={!isList}
+            isRequested={item.requested}
+            isJoined={item.joined}
+            numVotes={item.numVotes}
+            numVoters={item.numVoters}
+            leavePool={leavePool}
+            joinPool={joinPool}
+            cancelJoinPool={cancelPoolJoin}
+          />
+        )
+      }
     />
   );
 };
