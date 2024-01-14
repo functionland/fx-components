5cc4a4e1ba1c4d057f9912b2e0b74cfb303db1c5
hh <hhio618@gmail.com>
2024-01-14 17:54:45 +0330

    Add mock setup for testing

--- apps/box/src/api/bloxHardware.ts
+++ apps/box/src/api/bloxHardware.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { API_URL } from './index';
 import { TBloxProperty } from '../models';
+import { delay } from 'lodash';
 
 export type TBlox = {
   id: string; // peerId?
@@ -49,38 +50,63 @@ export const exchangeConfig = async (data: {
   peer_id?: string;
   seed?: string;
 }): Promise<{ data: { peer_id: string } }> => {
-  const formData = new URLSearchParams();
-  formData.append('peer_id', data?.peer_id);
-  formData.append('seed', data?.seed);
-  return axios.post(
-    `${API_URL}/peer/exchange?${formData.toString()}`,
-    undefined,
-    {
-      timeout: 1000 * 15,
-      headers: {
-        Accept: '*/*',
-        'Content-Type': 'application/x-www-form-urlencoded',
-      },
-    }
-  );
+  // const formData = new URLSearchParams();
+  // formData.append('peer_id', data?.peer_id);
+  // formData.append('seed', data?.seed);
+  // return axios.post(
+  //   `${API_URL}/peer/exchange?${formData.toString()}`,
+  //   undefined,
+  //   {
+  //     timeout: 1000 * 15,
+  //     headers: {
+  //       Accept: '*/*',
+  //       'Content-Type': 'application/x-www-form-urlencoded',
+  //     },
+  //   }
+  // );
+  console.log('exhcange called');
+  return {
+    data: { peer_id: '12D3KooWL1BuyxhJ5givbrR4Ym8SYpefLbD6nZRjoC5PzDCPtpQR' },
+  };
 };
 
 export const getBloxProperties = async (): Promise<any> => {
-  console.log(`${API_URL}/properties`);
-  const res = await axios.get(`${API_URL}/properties`);
-  console.log('res: ', res);
-  return res;
+  // console.log(`${API_URL}/properties`);
+  // const res = await axios.get(`${API_URL}/properties`);
+  // console.log('res: ', res);
+  // return res;
+  return {
+    data: {
+      name: 'Blox man',
+      hardwareID: 'hwd',
+      bloxFreeSpace: {
+        device_count: 1,
+        size: 1,
+        avail: 1,
+        used: 0,
+        used_percentage: 0,
+      },
+      containerInfo_fula: 'ci',
+      containerInfo_fxsupport: 'fxsupport',
+      containerInfo_node: 'node',
+      restartNeeded: 'false',
+      ota_version: 'v_ota',
+    },
+  };
 };
 /**
  * Erase partition
  * @returns
  */
 export const bloxFormatDisk = async (): Promise<{ data: GeneralResponse }> => {
-  return axios.post(`${API_URL}/partition`);
+  // return axios.post(`${API_URL}/partition`);
+  console.log('request format');
+  return { data: { status: true, msg: 'ok' } };
 };
 
 export const bloxDeleteFulaConfig = async (): Promise<{
   data: GeneralResponse;
 }> => {
-  return axios.post(`${API_URL}/delete-fula-config`);
+  // return axios.post(`${API_URL}/delete-fula-config`);
+  return { data: { status: true, msg: 'ok' } };
 };
